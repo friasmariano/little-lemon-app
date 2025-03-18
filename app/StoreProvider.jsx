50a76caf18a95083a685edcd567e227efbf01450
friasmariano <frias.mariano@gmail.com>
2025-03-18 09:43:17 -0400

    State Persistance - Integrated

--- app/StoreProvider.jsx
+++ app/StoreProvider.jsx
@@ -2,6 +2,7 @@
 
 import { useRef } from 'react'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 import { makeStore } from '../lib/store'
 import { increment } from '../lib/features/table-reservation/store/reservation-slice'
 
@@ -9,9 +10,19 @@ export default function StoreProvider({ children }) {
     const storeRef = useRef(null)
 
     if (!storeRef.current) {
-        storeRef.current = makeStore();
-        storeRef.current.dispatch(increment());
+        const { store, persistor } = makeStore();
+        storeRef.current = { store, persistor };
+        // storeRef.current = makeStore();
+        // storeRef.current.dispatch(increment());
     }
 
-    return <Provider store={storeRef.current}> {children} </Provider>
+    // return <Provider store={storeRef.current}> {children} </Provider>
+
+    return (
+        <Provider store={storeRef.current.store}>
+            <PersistGate loading={null} persistor={storeRef.current.persistor}>
+                {children}
+            </PersistGate>
+        </Provider>
+    )
 }
