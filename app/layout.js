5027f78a9a13405fc6cd104dc6d730dcf08bf2ec
friasmariano <frias.mariano@gmail.com>
2025-03-28 09:32:37 -0400

    Little Changes

--- app/layout.js
+++ app/layout.js
@@ -49,7 +49,7 @@ export default function RootLayout({ children }) {
               {children}
           </Main>
           <Footer />
-          </StoreProvider>
+        </StoreProvider>
       </body>
     </html>
   );
